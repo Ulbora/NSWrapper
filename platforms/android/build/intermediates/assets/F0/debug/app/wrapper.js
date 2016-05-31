5f1127b464e968c4b79b339185ceaef01e5789cd
ken <kenulborapublic@gmail.com>
2016-05-30 20:25:42 -0400

    Added Nativescript Toast and device plugin

--- platforms/android/build/intermediates/assets/F0/debug/app/wrapper.js
+++
@@ -1,8 +0,0 @@
-function pageLoaded(args) {
-    var page = args.object;   
-    var web = page.getViewById("webView");    
-    web.url = "http://10.0.2.2:3000";
-}
-exports.pageLoaded = pageLoaded;
-
-
--- platforms/android/src/main/assets/app/wrapper.js
+++ platforms/android/src/main/assets/app/wrapper.js
@@ -1,6 +1,14 @@
+var application = require("application");
+var context = application.android.context;
 function pageLoaded(args) {
     var page = args.object;   
-    var web = page.getViewById("webView");    
+    var web = page.getViewById("webView"); 
+    var androidSettings = web.android.getSettings();
+    androidSettings.setJavaScriptEnabled(true);
+    var device = new com.ulbora.device.Device;
+    var hostedToast = new com.ulbora.hosted.toast.HostedToast(context);
+    web.android.addJavascriptInterface(device, 'NSDevice');
+    web.android.addJavascriptInterface(hostedToast, 'HostedToast');
     web.url = "http://10.0.2.2:3000";
 }
 exports.pageLoaded = pageLoaded;
